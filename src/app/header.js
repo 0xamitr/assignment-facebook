19570d69b37eedf3eb4f1dccb1cf72598fda720d
0xamitr <0xamitr@gmail.com>
2024-08-31 22:03:24 +0530

    final

--- src/app/header.js
+++ src/app/header.js
@@ -1,29 +1,33 @@
 "use client"
 // components/CustomLogin.js
 import { signIn, signOut } from 'next-auth/react';
+import styles from './header.module.css'
+import { useSession } from 'next-auth/react';
 
 const Header = () => {
+    const { data: session } = useSession();
     const handleFacebookLogin = () => {
-        signIn('facebook'); // This triggers the OAuth flow with Facebook
+        signIn('facebook');
     };
     const handleLogout = async () => {
-        // Perform any additional logic if needed
-        // For example, clear local storage or other states
 
-        await signOut({ redirect: true, callbackUrl: '/' }); // Redirect after logout
+        await signOut({ redirect: true, callbackUrl: '/' });
     };
+    console.log()
 
     return (
-        <>
-            <div>
-                <button onClick={handleFacebookLogin}>Login with Facebook</button>
-            </div>
-            <div>
-                <button onClick={handleLogout}>
-                    Logout
-                </button>
-            </div>
-        </>
+        <div className={styles.header}>
+            {!session ?
+                <div>
+                    <button className={styles.btn} onClick={handleFacebookLogin}>Login</button>
+                </div> :
+                <div>
+                    <button className={styles.btn} onClick={handleLogout}>
+                        Logout
+                    </button>
+                </div>
+            }
+        </div>
     );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,14 @@ import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import axios from 'axios';
 import styles from './page.module.css'
+import { signIn, signOut } from 'next-auth/react';
 
 export default function Home() {
   const { data: session } = useSession();
   const [pagesdata, setPagesdata] = useState();
   const [individualpage, setIndividualpage] = useState();
   const [activepage, setActivepage] = useState()
+
   useEffect(() => {
     if (session) {
       const accessToken = session.accessToken;
@@ -40,8 +42,8 @@ export default function Home() {
           metric: 'page_follows,page_impressions,page_post_engagements,page_actions_post_reactions_total'
         },
       });
-      console.log('Page Data:', response.data.data); // Access data correctly
-      setIndividualpage(response.data.data); // Set the relevant data in state
+      console.log('Page Data:', response.data.data);
+      setIndividualpage(response.data.data);
     } catch (error) {
       console.error('Failed to fetch data from Facebook API:', error.response || error);
     }
@@ -51,59 +53,72 @@ export default function Home() {
   return (
     <main >
       {
-        session &&
-        <>
-          <img src={session.user.image} />
-          <p>Name: {session.user.name}</p>
-          <p>Email: {session.user.email}</p>
-          <div>
-            <h2>Pages</h2>
-            {
-              pagesdata &&
-              pagesdata.map((e, index) => {
-                return (
-                  <p onClick={() => getInfo(e)} key={index}>
-                    {e.name}
-                  </p>
-                )
-              })
-            }
-            {individualpage &&
-            <>
-              <h2>{activepage}</h2>
-              <div className={styles.container}>
-                <div className={styles.box}>
-                  <h3>Followers: {individualpage[1].values[1].value || 0}</h3>
-                  {/* <p>LifeTime: </p>
+        session ?
+          <>
+            <img className={styles.img} src={session.user.image} />
+            <p>Name: {session.user.name}</p>
+            <p>Email: {session.user.email}</p>
+            <div>
+              <h2>Pages</h2>
+              {
+                pagesdata &&
+                pagesdata.map((e, index) => {
+                  return (
+                    <p className={styles.pages} onClick={() => getInfo(e)} key={index}>
+                      {index+1}.  {e.name}
+                    </p>
+                  )
+                })
+              }
+              {individualpage &&
+                <>
+                  <h2>{activepage}</h2>
+                  <div className={styles.container}>
+                    <div className={styles.box}>
+                      <h3>Followers: {individualpage[1].values[1].value || 0}</h3>
+                      {/* <p>LifeTime: </p>
                   <p>This Month: </p>
                   <p>This Week: </p>
                   <p>Past Day: </p> */}
-                </div>
-                <div className={styles.box}>
-                  <h3>Page Reactions</h3>
-                  <p>LifeTime: {individualpage[0].values[0].value.value || 0}</p>
-                  <p>This Month: {individualpage[10].values[1].value.value || 0}</p>
-                  <p>This Week: {individualpage[7].values[1].value.value || 0}</p>
-                  <p>Past Day: {individualpage[4].values[1].value.value || 0}</p>
-                </div>
-                <div className={styles.box}>
-                  <h3>Page Impressions</h3>
-                  <p>This Month: {individualpage[8].values[1].value || 0}</p>
-                  <p>This Week: {individualpage[5].values[1].value || 0}</p>
-                  <p>Past Day: {individualpage[2].values[1].value || 0}</p>
-                </div>
-                <div className={styles.box}>
-                  <h3>Page Engagements</h3>
-                  <p>This Month: {individualpage[9].values[1].value || 0}</p>
-                  <p>This Week: {individualpage[6].values[1].value || 0}</p>
-                  <p>Past Day: {individualpage[3].values[1].value || 0}</p>
-                </div>
-              </div>
-            </>
-            }
-          </div>
-        </>
-      }
+                    </div>
+                    <div className={styles.box}>
+                      <h3>Page Reactions</h3>
+                      <p>LifeTime: {individualpage[0].values[0].value.value || 0}</p>
+                      <p>This Month: {individualpage[10].values[1].value.value || 0}</p>
+                      <p>This Week: {individualpage[7].values[1].value.value || 0}</p>
+                      <p>Past Day: {individualpage[4].values[1].value.value || 0}</p>
+                    </div>
+                    <div className={styles.box}>
+                      <h3>Page Impressions</h3>
+                      <p>This Month: {individualpage[8].values[1].value || 0}</p>
+                      <p>This Week: {individualpage[5].values[1].value || 0}</p>
+                      <p>Past Day: {individualpage[2].values[1].value || 0}</p>
+                    </div>
+                    <div className={styles.box}>
+                      <h3>Page Engagements</h3>
+                      <p>This Month: {individualpage[9].values[1].value || 0}</p>
+                      <p>This Week: {individualpage[6].values[1].value || 0}</p>
+                      <p>Past Day: {individualpage[3].values[1].value || 0}</p>
+                    </div>
+                  </div>
+                </>
+              }
+            </div>
+          </> :
+          <>
+            <button onClick={()=> signIn('facebook')} className={styles.mainbtn}>
+              <img
+                loading="lazy"
+                height="24"
+                width="24"
+                id="provider-logo"
+                src="https://authjs.dev/img/providers/facebook.svg"
+                alt="Facebook Logo"
+              />
+              <span>Sign in with Facebook</span>
+            </button>
+          </>
+}
     </main>
   );
 }
